5f6040497a22dd348f85b4af9f6bac5b6d73d56e
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-25 17:29:19 -0800

    final push on monday

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
     def index
-        pets = Pet.all 
-        render json: pets
+        pets = Pet.all
+        render json: pets, only: [:name, :owner, :image_url]
     end 
 end
