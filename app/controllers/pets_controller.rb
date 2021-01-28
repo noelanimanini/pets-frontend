54d5c73c45212f222bf565da84e3a91d6c2572d2
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-28 00:17:37 -0800

    needs NEEDs

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,16 @@ class PetsController < ApplicationController
         pets = Pet.all
         render json: pets, only: [:id, :name, :owner, :image_url, :medication, :exercise, :diet]
     end 
+
+    def update
+        pet = Pet.find(params[:id])
+        pet.update(pet_params)
+        render json: pet
+    end 
+
+    private
+
+    def pet_params
+        params.require(:pet).permit(:medication, :exercise, :diet)
+    end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,21 @@ p1 = Pet.create(name: "Flopsie", owner: "Fiona", image_url: "https://static.wiki
 
 p2 = Pet.create(name: "Donkey", owner: "Shrek", image_url: "https://static.wikia.nocookie.net/universalstudios/images/f/f0/Donkey_shrek.png/revision/latest?cb=20170921231940", medication: "ear drops", exercise: "pool time twice a day", diet: "Waffles")
 
+p3 = Pet.create(name: "Flopsie", owner: "Fiona", image_url: "https://static.wikia.nocookie.net/avatar/images/4/4c/Goat_gorilla.png/revision/latest/scale-to-width-down/333?cb=20130706150318", medication: "eye drops", exercise: "walk twice a day", diet: "Science Hill Diet")
+
+p4 = Pet.create(name: "Donkey", owner: "Shrek", image_url: "https://static.wikia.nocookie.net/universalstudios/images/f/f0/Donkey_shrek.png/revision/latest?cb=20170921231940", medication: "ear drops", exercise: "pool time twice a day", diet: "Waffles")
+
+p5 = Pet.create(name: "Flopsie", owner: "Fiona", image_url: "https://static.wikia.nocookie.net/avatar/images/4/4c/Goat_gorilla.png/revision/latest/scale-to-width-down/333?cb=20130706150318", medication: "eye drops", exercise: "walk twice a day", diet: "Science Hill Diet")
+
+p6 = Pet.create(name: "Donkey", owner: "Shrek", image_url: "https://static.wikia.nocookie.net/universalstudios/images/f/f0/Donkey_shrek.png/revision/latest?cb=20170921231940", medication: "ear drops", exercise: "pool time twice a day", diet: "Waffles")
+
 n1 = Need.create(notes: "Flopsie was very nice to the other animals", medication: true, exercise: true, diet: false, pet_id: p1.id )
 n2 = Need.create(notes: "Donkey is having fun in the pool!", medication: true, exercise: true, diet: false, pet_id: p2.id)
+n3 = Need.create(notes: "Flopsie was very nice to the other animals", medication: true, exercise: true, diet: false, pet_id: p3.id )
+n4 = Need.create(notes: "Donkey is having fun in the pool!", medication: true, exercise: true, diet: false, pet_id: p4.id)
+n5 = Need.create(notes: "Flopsie was very nice to the other animals", medication: true, exercise: true, diet: false, pet_id: p5.id )
+n6 = Need.create(notes: "Donkey is having fun in the pool!", medication: true, exercise: true, diet: false, pet_id: p6.id)
+
 
 
 puts 'done'
